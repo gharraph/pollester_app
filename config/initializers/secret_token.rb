42ab72e2265156bb0249bb0d9b9c7adfef779603
Sherief Gharraph <gharraph@msn.com>
2012-07-30 10:49:00 -0700

    first commit. just created the new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PollesterApp::Application.config.secret_token = '8c1a028f940c3ed306e2d996429f9c3264f6c79a902eb857aa7c1bbfaf872f2b26d0d344a1f031e63d38ce8e26e7e32630f5897c0f382f46b3328b0140f28a72'
